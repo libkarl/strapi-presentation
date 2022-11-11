5eae9ede0d00ba02417ca24e834df09878d4aaa4
Charles <karel.hb@email.cz>
2022-11-11 18:44:36 +0100

    repair blog slider

--- packages/web/src/components/Generics/Blog.tsx
+++ packages/web/src/components/Generics/Blog.tsx
@@ -32,67 +32,70 @@ const BlogSlider = (props: ArchiveProps) => {
             }}
             className="swiper-wrapper pb-70 pt-5"
           >
-            {data.map((item, i) => (
-              <SwiperSlide key={"blog_" + item.id}>
-                <div className="swiper-slide active">
-                  <div className="block-news">
-                    <div className="item-news">
-                      <div className="row">
-                        <div className="col-xl-6 col-lg-12">
-                          <div className="img-news">
-                            <Link href="/blog-single">
-                              <a>
-                                <img
-                                  src={
-                                    process.env.NEXT_PUBLIC_STRAPI_URL +
-                                    item.attributes.bigpicture?.data.attributes
-                                      .url
-                                  }
-                                  alt="Agon"
-                                />
-                              </a>
-                            </Link>
+            {data.map((item, i) => {
+              const uniquePath = `/articles/${item.id}`;
+              return (
+                <SwiperSlide key={"blog_" + item.id}>
+                  <div className="swiper-slide active">
+                    <div className="block-news">
+                      <div className="item-news">
+                        <div className="row">
+                          <div className="col-xl-6 col-lg-12">
+                            <div className="img-news">
+                              <Link href={uniquePath}>
+                                <a>
+                                  <img
+                                    src={
+                                      process.env.NEXT_PUBLIC_STRAPI_URL +
+                                      item.attributes.bigpicture?.data
+                                        .attributes.url
+                                    }
+                                    alt="Agon"
+                                  />
+                                </a>
+                              </Link>
+                            </div>
                           </div>
-                        </div>
-                        <div className="col-xl-6 col-lg-12">
-                          <div className="right-news card-grid-style-4 mb-0">
-                            <span className="tag-dot">
-                              {item.attributes.category}
-                            </span>
-                            <Link href="/blog-single">
-                              <a className="text-heading-4">
-                                {item.attributes.articlename}
-                              </a>
-                            </Link>
+                          <div className="col-xl-6 col-lg-12">
+                            <div className="right-news card-grid-style-4 mb-0">
+                              <span className="tag-dot">
+                                {item.attributes.category}
+                              </span>
+                              <Link href={uniquePath}>
+                                <a className="text-heading-4">
+                                  {item.attributes.articlename}
+                                </a>
+                              </Link>
 
-                            <p className="text-body-text color-gray-500 text-desc-fix-h">
-                              {item.attributes.articleintro}
-                            </p>
-                            <div className="blog-img-user">
-                              <div className="img-user img-user-round">
-                                <img
-                                  src={
-                                    process.env.NEXT_PUBLIC_STRAPI_URL +
-                                    item.attributes.foto?.data.attributes.url
-                                  }
-                                  alt="Agon"
-                                />
-                              </div>
-                              <h4 className="text-heading-6 color-gray-900">
-                                {item.attributes.author}
-                              </h4>
-                              <p className="text-body-small color-gray-500">
-                                {item.attributes.date}
+                              <p className="text-body-text color-gray-500 text-desc-fix-h">
+                                {item.attributes.articleintro}
                               </p>
+                              <div className="blog-img-user">
+                                <div className="img-user img-user-round">
+                                  <img
+                                    src={
+                                      process.env.NEXT_PUBLIC_STRAPI_URL +
+                                      item.attributes.foto?.data.attributes.url
+                                    }
+                                    alt="Agon"
+                                  />
+                                </div>
+                                <h4 className="text-heading-6 color-gray-900">
+                                  {item.attributes.author}
+                                </h4>
+                                <p className="text-body-small color-gray-500">
+                                  {item.attributes.date}
+                                </p>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            ))}
+                </SwiperSlide>
+              );
+            })}
           </Swiper>
         </div>
         <div className="swiper-button-next swiper-button-next-5" />
